221552d312a6b2ca563a47aae11e87e608f963c3
austynpark <68501510+austynpark@users.noreply.github.com>
2022-05-18 16:12:08 -0700

    obj loader / mesh startup code

---
+++ OuroborosEngine/OuroborosRenderer/mesh.cpp
@@ -0,0 +1,24 @@
+#include "mesh.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#define TINYOBJLOADER_IMPLEMENTATION
+
+#include <unordered_map>
+#include <tinyobjloader/tiny_obj_loader.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
+
+namespace std {
+    template<> struct hash<Vertex> {
+        size_t operator()(Vertex const& vertex) const {
+            return ((hash<glm::vec3>()(vertex.position) ^ (hash<glm::vec3>()(vertex.normal) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.uv) << 1);
+        }
+    };
+}
+
+bool Mesh::load_asset(const char* file_name)
+{
+
+
+    return false;
+}
